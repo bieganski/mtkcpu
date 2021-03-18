718a636b37f03e2602dc8e884e13c6440e9c568b
Matuesz <bieganski.m@wp.pl>
2021-03-18 14:49:25 +0100

    added silent and verbose testing mode

--- mtkcpu/test_cpu.py
+++ mtkcpu/test_cpu.py
@@ -4,11 +4,14 @@ from cpu import MtkCpu
 from tests.reg_tests import REG_TESTS
 
 
-def reg_test(name, asm_str, reg_num, exptected_val, timeout_cycles, reg_init=[]):
+def reg_test(name, asm_str, reg_num, exptected_val, timeout_cycles, reg_init=[], verbose=False):
     from io import StringIO
     source_file = StringIO(asm_str)
     from asm_dump import dump_asm
-    cpu = MtkCpu(reg_init=reg_init, mem_init=dump_asm(source_file))
+    cpu = MtkCpu(reg_init=reg_init, mem_init=dump_asm(source_file, verbose=verbose))
+
+
+    LOG = lambda x : print(x) if verbose else True
 
     sim = Simulator(cpu)
     sim.add_clock(1e-6)
@@ -19,10 +22,10 @@ def reg_test(name, asm_str, reg_num, exptected_val, timeout_cycles, reg_init=[])
         for _ in range(timeout_cycles):
             en = yield cpu.reg_write_port.en
             if en == 1:
-                print("___ en detected ")
+                LOG("___ en detected ")
                 addr = yield cpu.reg_write_port.addr
                 if addr == reg_num:
-                    print(f"___ got write to reg {addr}...")
+                    LOG(f"___ got write to reg {addr}...")
                     val = yield cpu.reg_write_port.data
                     if val != exptected_val:
                         print(f"== ERROR: Expected data write to reg x{addr} of value {exptected_val}," 
