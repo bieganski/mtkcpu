736ff2d89b34e68c3f659eae6563fe32b4661059
Matuesz <bieganski.m@wp.pl>
2021-03-24 21:55:00 +0100

    sim cleaned up; split by two processes (active and passive mem.)

---
+++ mtkcpu/tests/branch_tests.py
@@ -0,0 +1,3 @@
+BRANCH_TESTS = [
+    
+]
---
+++ mtkcpu/tests/mem_tests.py
@@ -0,0 +1,24 @@
+# MEM_TESTS semantic:
+# after executing t['source'], check whether in register xN (N = t['out_reg']) is value t['out_val'],
+# providing that initial register state was: val(xN) = t['reg_init'][N] 
+# and initial memory state: val(addr) = mem_init['addr'], check whether after executing t['source']
+# in register xN (N = t['out_reg']) is value t['out_val'], and for all k, v in out_mem.items(): mem[k] == v.
+# If t['out_val'] or t['out_mem'] is null, skip according check.
+
+MEM_TESTS = [
+    
+    {
+        "name": "simple 'lw'",
+        "source": 
+        """
+        .section code
+            addi, x10, x0, 0xde
+            lw x10, 0(xde)
+        """,
+        "out_reg": 10,
+        "out_val": 0xdeadbeef,
+        "timeout": 9000,
+        "mem_init": {0xde: 0xdeadbeef},
+        "out_mem": {} # empty dict means whatever (no memory checks performed)
+    },
+]
