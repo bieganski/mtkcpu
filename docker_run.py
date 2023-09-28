d21a4912fd4efe58652a65338359c95f3a3f257a
Mateusz <35270527+bieganski@users.noreply.github.com>
2023-09-28 19:57:26 +0200

    docker: Add bitstream synthesis capability (requires yosys and nextpnr present in container). (#56)

---
+++ docker_run.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+
+from typing import Optional
+from pathlib import Path
+import sys
+import grp
+import subprocess
+import argparse
+
+class Color:
+    yellow = "\x1b[33m"
+    green = "\x1b[32m"
+    red = "\x1b[21m"
+    bold_red = "\x1b[31;1m"
+    bold = "\033[1m"
+    uline = "\033[4m"
+    reset = "\x1b[0m"
+
+def warn(msg: str):
+    sys.stderr.write(f"{Color.bold}WARNING{Color.reset}: {msg}\n")
+
+def get_group_id(group_name: str) -> Optional[int]:
+    try:
+        group_info = grp.getgrnam(group_name)
+    except Exception:
+        return None
+    return group_info.gr_gid
+
+def get_group_members(group_name: str) -> list[str]:
+    if get_group_id(group_name) is None:
+        return []
+    return grp.getgrnam(group_name).gr_mem
+
+def construct_groups_params(groups: list[str] = ["dialout", "plugdev"]):
+    res = " "
+    for name in groups:
+        id = get_group_id(name)
+        if id is None:
+            warn(f"Could not find group '{name}' in /etc/group! In order to avoid UART/JTAG connection issues, please add the group manually, set your user as a member and refresh (As a result command 'groups' should print it)")
+            continue
+        res += f"--group-add {id} "
+        
+    return res
+
+def main(cmd : Optional[str]):
+    container_name = "docker.io/library/mtkcpu:1.0.0"
+    interactive = "" if cmd else "-it"
+    if not cmd:
+        cmd = ""
+    else:
+        if ';' in cmd:
+            warn("\n\nYour command contain ';' - probably it won't work as you might expect. Use && instead.\n\n")
+        if (not cmd.startswith("sh")) and (not cmd.startswith("bash")):
+            cmd = f"sh -c '{cmd}'"
+
+    command = f"""
+docker run \
+--net host \
+{construct_groups_params()} \
+-v {Path(__file__).parent}/sw:/toolchain/sw \
+{interactive} {container_name} {cmd}
+    """
+    print(command)
+
+    subprocess.run(command, shell=True)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(usage="Runs interactive bash session inside Plumerai-Demo docker container.")
+    parser.add_argument("--cmd", type=str)
+    main(**vars(parser.parse_args()))
